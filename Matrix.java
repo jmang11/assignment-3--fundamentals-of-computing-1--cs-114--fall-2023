05eababe0b1e9cc752c638f7db3a4ee67d23cc90
jmang11 <mangiamel@hartford.edu>
2023-11-13 14:19:19 -0500

    code

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,85 @@
 public class Matrix {
+
+  public static int[][] matrix;
+  public static int row;
+  public static int column;
+
+  static void matrix(int size) {
+
+    System.out.println();
+    matrix = new int[size][size];
+
+    System.out.println("Your matrix is " + size + " x " + size);
+    System.out.println();
+    System.out.println("Printing matrix with default values:");
+    }
+
+  static void printMatrix(){
+
+    final int Length = matrix.length;
+
+    for (row = 0; row < Length ; row++){
+      for (column = 0; column < Length ; column++){
+        if(row + column == Length-1){
+          System.out.print("\u001B[33m" + matrix[row][column] + "\u001B[0m" + "\t");
+        }else if ( row + column > Length-1){
+          System.out.print(matrix[row][column] + "\t");
+        }else if(row + column < Length-1){
+          System.out.print(matrix[row][column] + "\t");
+        }
+    }
+    System.out.println();
+  }
+}
+
+  static void populatedMatrix(){
+
+    final int Length = matrix.length;
+
+    System.out.println();
+    System.out.println("Populating matrix ... matrix populated");
+    System.out.println();
+    System.out.println("Printing populated matrix:");
+
+    for(row = 0; row < Length; row++){
+      for(column = 0; column < Length; column++){
+        matrix[row][column] = ((row * Length) + column +1);
+      }
+    }
+      System.out.println();
+    for(row = 0; row < Length; row++){
+      for(column = 0; column < Length; column++){
+      }
+    }
+}
+
+  private static void swap(int input_1, int input_2, int input_3, int input_4){
+
+    int x1 = input_1;
+    int x2 = input_3;
+    int y1 = input_2;
+    int y2 = input_4;
+
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+
+  static void flipMatrix(){
+
+    final int Length = matrix.length;
+
+    System.out.println();
+    System.out.println("Flipping matrix ... matrix flipped");
+    System.out.println();
+    System.out.println("Printing flipped matrix");
+
+    for(row = 0; row < Length; row++){
+      for(column = 0; column < Length; column++){
+        if(row + column < Length - 1){
+          swap(row, column, Length - 1 - row, Length - 1 - column);
+        }else if(row + column == Length){}
+      }
+    }
+}
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,32 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Please enter the size of your matrix:");
+    int userInput = scan.nextInt();
+    System.out.println();
+
+    try {
+      if(userInput > 0){
+        }
+          else{
+            System.out.println("Please re-run the code and enter a Positive Integer.");
+          }
+    }
+      catch (Exception e) {
+        System.out.println("Please re-run the code and enter a Positive Integer.");
+      }
+
+    Matrix.matrix(userInput);
+    Matrix.printMatrix();
+    Matrix.populatedMatrix();
+    Matrix.printMatrix();
+    Matrix.flipMatrix();
+    Matrix.printMatrix();
+
+    scan.close();
+
+    }
   }
-}
